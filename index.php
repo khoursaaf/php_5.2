54458bee3537831d474824418d6a8d7116904eec
khoursaaf <khoursa@gmail.com>
2018-11-26 16:44:33 +0100

    first commit

---
+++ index.php
@@ -0,0 +1,14 @@
+<!DOCTYPE html>
+<html>
+    <head>
+        <meta charset="utf-8" />
+        <title>Exercise</title>
+    </head>
+    <body>
+      <p>Avec le tableau de l'exercice 1, afficher la valeur de la troisième ligne de ce tableau.</p>
+      <?php
+      $months = ['janvier','février','mars','avril','mai','juin','juillet','aout','septembre','octobre','novembre','décembre'];
+       ?>
+      <p>La valeur de la troisième ligne de ce tableau est : <?php echo $months[2]; ?>.</p>
+    </body>
+</html>
